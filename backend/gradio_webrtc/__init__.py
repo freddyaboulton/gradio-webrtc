a288a1b9f2bf871e437640139ba3cf0098c0b130
freddyaboulton <alfonsoboulton@gmail.com>
2024-12-06 10:56:14 -0500

    add util

--- backend/gradio_webrtc/__init__.py
+++ backend/gradio_webrtc/__init__.py
@@ -4,7 +4,7 @@ from .credentials import (
     get_twilio_turn_credentials,
 )
 from .reply_on_pause import AlgoOptions, ReplyOnPause, SileroVadOptions
-from .utils import AdditionalOutputs, audio_to_bytes, audio_to_file
+from .utils import AdditionalOutputs, audio_to_bytes, audio_to_file, audio_to_float32
 from .webrtc import StreamHandler, WebRTC
 
 __all__ = [
@@ -12,6 +12,7 @@ __all__ = [
     "AdditionalOutputs",
     "audio_to_bytes",
     "audio_to_file",
+    "audio_to_float32",
     "get_hf_turn_credentials",
     "get_twilio_turn_credentials",
     "get_turn_credentials",
