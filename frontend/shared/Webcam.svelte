b6d8e6a57ff56a42a6b2fc1f78ed70ac3df6f7f6
Freddy Boulton <alfonsoboulton@gmail.com>
2025-01-27 16:16:42 -0500

    Fix (#52)
    Co-authored-by: Freddy Boulton <freddyboulton@hf-freddy.local>

--- frontend/shared/Webcam.svelte
+++ frontend/shared/Webcam.svelte
@@ -135,6 +135,7 @@
                         case "connected":
                             stream_state = "open";
                             _time_limit = time_limit;
+							dispatch("tick");
                             break;
                         case "disconnected":
                             stream_state = "closed";
